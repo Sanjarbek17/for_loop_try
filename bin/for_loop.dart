8596cb9280e0b2e9c3ae301eafd3c76cff7827c5
sanjarbek <sanjarsaidov173@gamil.com>
2022-07-16 14:54:03 +0500

    done

--- bin/for_loop.dart
+++
@@ -1,5 +0,0 @@
-import 'package:for_loop/for_loop.dart' as for_loop;
-
-void main(List<String> arguments) {
-  print('Hello world: ${for_loop.calculate()}!');
-}
--- test/for_loop_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:for_loop/for_loop.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
